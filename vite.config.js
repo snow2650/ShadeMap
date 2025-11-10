e04b0fe0e3ca35578e7baca2b30ae0f2db23aee1
snow2650 <snow113@students.cs.ubc.ca>
2025-11-10 03:07:15 -0800

    Organized structure

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//https://github.com/snow2650/ShadeMap.git
 export default defineConfig({
   plugins: [react()],
+  base: '/ShadeMap/'
 })
